b2e049455a65a116606cff91b3f370769f20d637
judith <8w8u8@naver.com>
2024-10-07 12:15:15 +0900

    fix: 어노테이션 추가

--- community/src/test/java/efub/assignment/community/account/AccountRepositoryTest.java
+++ community/src/test/java/efub/assignment/community/account/AccountRepositoryTest.java
@@ -4,6 +4,7 @@ import efub.assignment.community.account.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class AccountRepositoryTest {
 
--- community/src/test/java/efub/assignment/community/account/domain/AccountTest.java
+++ community/src/test/java/efub/assignment/community/account/domain/AccountTest.java
@@ -2,9 +2,15 @@ package efub.assignment.community.account.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class AccountTest {
 
     @Test
